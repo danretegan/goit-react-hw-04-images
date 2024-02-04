a2494dd49dcf528809d06265926432dba21769eb
Dan <danretegan@yahoo.com>
2024-02-04 06:38:41 +0200

    refactoring App.jsx
    Used React hooks instead of class based components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,100 +1,91 @@
-import React, { Component } from 'react';
+// App.jsx
+import React, { useState, useEffect, useCallback } from 'react';
 import Searchbar from './searchbar/Searchbar';
 import Loader from './loader/Loader';
 import ImageGallery from './imageGallery/ImageGallery';
 import ScrollButton from './scrollButton/ScrollButton';
 import Button from './loadMoreButton/LoadButton';
 import Modal from './modal/Modal';
-import fetchImages from './services/pixabayApi';
 
-class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    error: null,
-    query: '',
-    page: 1,
-    showModal: false,
-    selectedImage: null,
-    isLastPage: false,
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLastPage, setIsLastPage] = useState(false);
 
-  componentDidUpdate(_prevProps, prevState) {
-    if (prevState.query !== this.state.query) {
-      this.setState({ images: [], page: 1, isLastPage: false }, () => {
-        this.fetchImagesApp();
-      });
+  const fetchImagesApp = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const apiKey = '34187261-edb3bdfe414ee3b7adebeccc5';
+      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${page}&per_page=10`;
+
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.hits.length === 0) {
+        setIsLastPage(true);
+        return;
+      }
+
+      setImages(prevImages => [...prevImages, ...data.hits]);
+      setPage(prevPage => prevPage + 1);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  }, [query, page]);
 
-  // Această metodă gestionează obținerea de imagini din pixabyAPI.js folosind funcția fetchImages. Parametrii precum query (cuvântul cheie de căutare) și page (numărul de pagină) sunt obținuți din starea locală a componentei. Callback-urile sunt utilizate pentru a actualiza starea componentei App în funcție de rezultatele obținute din API.
-  // - updatedImages: array-ul de imagini actualizate. Imaginile noi sunt adăugate la array-ul existent de imagini.
-  // - newPage: noul număr de pagină, eventual actualizat după obținerea imaginilor.
-  // - isLastPage: valoarea booleană care indică dacă s-a ajuns la ultima pagină de rezultate.
-  // - isLoading: valoarea booleană care indică dacă încărcarea este în desfășurare.
-  // - error: mesajul de eroare, în cazul în care există probleme în timpul apelului API.
-  fetchImagesApp = () => {
-    const { query, page } = this.state;
-
-    fetchImages(
-      query,
-      page,
-      updatedImages =>
-        this.setState(prevState => ({
-          images: [...prevState.images, ...updatedImages],
-        })),
-      newPage => this.setState({ page: newPage }),
-      isLastPage => this.setState({ isLastPage }),
-      isLoading => this.setState({ isLoading }),
-      error => this.setState({ error })
-    );
-  };
-
-  handleSearchSubmit = query => {
-    if (this.state.query === query) {
+  const handleSearchSubmit = newQuery => {
+    if (query === newQuery) {
       return;
     }
-    this.setState({
-      query: query,
-      page: 1,
-      images: [],
-      error: null,
-      isLastPage: false,
-    });
+
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
+    setError(null);
+    setIsLastPage(false);
   };
 
-  handleImageClick = image => {
-    this.setState({ selectedImage: image, showModal: true });
+  const handleImageClick = useCallback(image => {
+    setSelectedImage(image);
+    setShowModal(true);
     document.body.style.overflow = 'hidden';
-  };
+  }, []);
 
-  handleModalClose = () => {
-    this.setState({ selectedImage: null, showModal: false });
+  const handleModalClose = useCallback(() => {
+    setSelectedImage(null);
+    setShowModal(false);
     document.body.style.overflow = 'auto';
-  };
+  }, []);
 
-  render() {
-    const { images, isLoading, error, showModal, selectedImage, isLastPage } =
-      this.state;
+  useEffect(() => {
+    if (query && (page === 1 || isLastPage)) {
+      setImages([]);
+      fetchImagesApp();
+    }
+  }, [query, page, isLastPage, fetchImagesApp]);
 
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        {error && <p>Error: {error}</p>}
-        <ImageGallery images={images} onItemClick={this.handleImageClick} />
-        {isLoading && <Loader />}
-        {!isLoading && images.length > 0 && !isLastPage && (
-          <Button onClick={this.fetchImagesApp} />
-        )}
+  return (
+    <>
+      <Searchbar onSubmit={handleSearchSubmit} />
+      {error && <p>Error: {error}</p>}
+      <ImageGallery images={images} onItemClick={handleImageClick} />
+      {isLoading && <Loader />}
+      {!isLoading && images.length > 0 && !isLastPage && (
+        <Button onClick={() => fetchImagesApp()} />
+      )}
 
-        {showModal && (
-          <Modal image={selectedImage} onClose={this.handleModalClose} />
-        )}
+      {showModal && <Modal image={selectedImage} onClose={handleModalClose} />}
 
-        <ScrollButton />
-      </>
-    );
-  }
-}
+      <ScrollButton />
+    </>
+  );
+};
 
 export default App;
--- src/components/services/pixabayApi.js
+++ src/components/services/pixabayApi.js
@@ -1,37 +1,31 @@
+// pixabayApi.js
 import pixabayService from './pixabayService';
 
-// Funcția pentru obținerea imaginilor din API-ul Pixabay:
 const fetchImages = async (
-  query, // Cuvântul cheie pentru căutare
-  page, // Numărul paginii de rezultate căutate (implicit 1)
-  updateImages, // Funcția pentru actualizarea imaginilor în starea componentei principale (App)
-  setPage, // Funcția pentru actualizarea numărului paginii în starea componentei
-  setIsLastPage, // Funcția pentru actualizarea indicatorului ultimei pagini în starea componentei
-  setIsLoading, // Funcția pentru actualizarea stării de încărcare în starea componentei
-  setError // Funcția pentru gestionarea erorilor în starea componentei
+  query,
+  page,
+  updateImages,
+  setPage,
+  setIsLastPage,
+  setIsLoading,
+  setError
 ) => {
   setIsLoading(true);
 
   try {
-    // Încărcare imaginilor de la API
     const { images: fetchedImages, totalHits } =
       await pixabayService.searchImages(query, page);
 
-    // Așteptare 1 secundă după ce imaginile au fost încărcate
     await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // Actualizăm imaginile în starea componentei, adăugând imaginile noi la cele existente
     updateImages(fetchedImages);
 
-    // Actualizăm numărul paginii în starea componentei, trecând la pagina următoare
     setPage(page + 1);
 
-    // Actualizăm indicatorul ultimei pagini în funcție de condiția specificată
     setIsLastPage(fetchedImages.length >= totalHits);
   } catch (error) {
     setError(error.message);
   } finally {
-    // Oprim Loader-ul după 1 secundă
     await new Promise(resolve => setTimeout(resolve, 1000));
     setIsLoading(false);
   }
